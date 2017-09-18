d1c75e192a5ac38e14cb7590d21cf9e9fc5e8805
rhysfrank <32052134+rhysfrank@users.noreply.github.com>
2017-09-18 14:43:30 -0400

    Add files via upload

---
+++ step_1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class step_1 : MonoBehaviour {
+
+
+	float time; 
+	//float uses a decimal for the time
+	int myScore; 
+	//int means only normal numbers 
+	bool printedMessage;
+	//bool has to be true or false
+
+	// Use this for initialization 
+	void Start () {
+			print (“The game has begun!”);
+
+			printedMessage = false; 
+		}
+
+		// Update is called once per frame
+		void Update () { 
+			if (Time.time  > 3 && printedMessage == false) {
+			print (“It has been three seconds!”); 
+				printedMessage = true;
+		}
+	}
+}
+		
+	
+
+
+
